ec5e9505ff009d1dc8e95d0aca21118007667269
Aasik <aasikcs@gmail.com>
2019-07-09 18:08:37 +0530

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -5,7 +5,7 @@ $mysql_password = "";            //password for the username
 $mysql_db = "simple_login";       //database to be used for the process
 
 $db = mysqli_connect($mysql_hostname,$mysql_username,$mysql_password) or die("could not connect to database"); // check the connection availability for the database
-$mysqli_select_db = ($mysql_db,$db) or die("could not select database"); // check the availability of the database in the DBMS
+$mysqli_select_db = ($db,$mysql_db) or die("could not select database"); // check the availability of the database in the DBMS
 
 
 
